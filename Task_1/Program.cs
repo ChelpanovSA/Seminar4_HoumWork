6820da5e5c7c523bf068f539385422b1375adcad
Сергей Челпанов <S.inter.4@mail.ru>
2023-02-14 18:57:27 +0300

    Task 1/2

---
+++ Task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿//*Задача 25: Напишите функцию, которая принимает на вход два числа (A и B) и возводит число A 
+// в натуральную степень B. 3, 5 -> 243 (3⁵); 2, 4 -> 16.
+
+int Extent(int A, int B)
+{
+    int result = 1;
+    for (int count = 1; count <= B; count++)
+    {
+        result = result * A;
+    }
+    return result;
+}
+int ReadConsole(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine()!);
+}
+
+Console.Clear();
+int A = ReadConsole("Введите число А:");
+int B = ReadConsole("Введите число B:");
+int pow = Extent(A, B);
+Console.Write(pow);
---
+++ Task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// *Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11; 82 -> 10; 9012 -> 12/*
+
+void Dellet(string[] args)
+{
+        Console.WriteLine("Введите число");
+    int numA = int.Parse(Console.ReadLine()!);
+    int result = 0;
+    int number = 0;
+        {
+        int numB = numA;
+        while (numB > 0)
+        {
+            number = numB % 10;
+            result = result + number;
+            numB = numB / 10;
+        }
+    }
+    Console.WriteLine($"Сумма цифр {numA} равна {result}");
+}
+
+Dellet(args);
